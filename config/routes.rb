17f88ec96f133fd126b3e57b68678b6919f697de
Antoine Magny <mr.magny+github@gmail.com>
2015-06-15 16:46:06 +0200

    gem and route change

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  post 'index' to 'castles/index'
 
   root 'welcome#home'
   # Example of regular route:
