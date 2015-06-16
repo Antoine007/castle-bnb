cedfdca273c76d3215377c80aa82b9f200847fc0
Antoine Magny <mr.magny+github@gmail.com>
2015-06-16 17:51:42 +0200

    new user form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :phone_number, :address, :gender) } # The :firstname and :lastname are my custom fields.
+  end
+
+  protected
+
+  # def after_sign_in_path_for(id)
+  #   "user/id"
+  # end
 end
