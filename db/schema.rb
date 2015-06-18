4e75416567dea965409d87e98b38caa0e62fae57
Robert Johnston <rfjohnstonunicef@gmail.com>
2015-06-18 14:47:10 +0200

    added price to Castle

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618102938) do
+ActiveRecord::Schema.define(version: 20150618124521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,19 @@ ActiveRecord::Schema.define(version: 20150618102938) do
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+    t.integer  "price"
   end
+
+  create_table "nights", force: :cascade do |t|
+    t.integer  "castle_id"
+    t.integer  "booking_id"
+    t.date     "not_available"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "nights", ["booking_id"], name: "index_nights_on_booking_id", using: :btree
+  add_index "nights", ["castle_id"], name: "index_nights_on_castle_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -73,4 +85,6 @@ ActiveRecord::Schema.define(version: 20150618102938) do
 
   add_foreign_key "bookings", "castles"
   add_foreign_key "bookings", "users"
+  add_foreign_key "nights", "bookings"
+  add_foreign_key "nights", "castles"
 end
